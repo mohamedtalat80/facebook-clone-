adf361bea8cb0f84e786e4d8fe10a2e8c9fa21cc
mohamed <abnmoh525@gmail.com>
2025-03-05 22:04:22 +0200

    allowing vercel to be a host

--- user_model/settings.py
+++ user_model/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(_3=(@%x)ic!c=0_gy(%db%g-im)4$y#!k$!i41-w1y-vk%dg6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
