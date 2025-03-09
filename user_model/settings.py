2d913523b60fc204cef7c616c66d1c4233e30753
mohamedtalat80 <80793810+mohamedtalat80@users.noreply.github.com>
2025-03-10 00:15:26 +0200

    Update settings.py

--- user_model/settings.py
+++ user_model/settings.py
@@ -77,9 +77,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # 'allauth.account.middleware.AccountMiddleware',
-    whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'user_model.urls'
 
 TEMPLATES = [
